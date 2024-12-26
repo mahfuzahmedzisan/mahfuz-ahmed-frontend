f089843b7b12e55542e6efc13ecd3b3b41b95e43
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-12-27 00:21:42 +0600

    Navbar --done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,48 @@ const config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        '3xl': '1536px',
+      }
+    },
     extend: {
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1024px',
+        '2xl': '1280px',
+        '3xl': '1536px',
+      },
       colors: {
-        dark: "var(--dark)",
-        white: "var(--white)",
+        'primary': "#ff014f",
+
+        'white': "#ffffff",
+        'light': "#c4cfde",
+
+        'bg-dark': "#212428",
+        'dark-primary': "#1e2024",
+        'dark-secondary': "#23272b",
+
+        'shadow-color-1': "#1c1e22",
+        'shadow-color-2': "#262a2e",
+
+      },
+      backgroundImage: {
+        'gradient-primary': 'linear-gradient(145deg, #1e2024, #23272b)',  
+      },
+      boxShadow: {
+        'shadow-primary': "10px 10px 19px #1c1e22, -10px -10px 19px #262a2e",
+      },
+
+      fontFamily: {
+        'Poppins': "'Poppins', sans-serif",
+        'Roboto': "'Roboto', sans-serif",
+        'Montserrat': "'Montserrat', sans-serif",
       },
     },
   },
